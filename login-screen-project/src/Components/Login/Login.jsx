570f7376ba003b70b4f3b21bb945f27b98d64cac
lirajoaop <joaopedroababa132@gmail.com>
2024-05-29 14:52:07 -0300

    projeto estilizado

--- login-screen-project/src/Components/Login/Login.jsx
+++ login-screen-project/src/Components/Login/Login.jsx
@@ -15,12 +15,12 @@ const Login = () => {
     <div className="container">
         <form onSubmit={handleSubmit}>
             <h1>Acesse o sistema</h1>
-            <div>
+            <div className='input-field'>
                 <input type="email" placeholder='E-mail'
                 onChange={(e) => setUsername(e.target.value)}/>
                 <FaUser className='icon' />
             </div>
-            <div>
+            <div className='input-field'>
                 <input type="password" placeholder='Senha'
                 onChange={(e) => setPassword(e.target.value)} />
                 <FaLock className='icon' />
